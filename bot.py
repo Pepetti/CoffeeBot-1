8f9602a68550daa70f1328173e67488e1a3fd64e
oskari <oskari.kotajarvi@gmail.com>
2018-11-03 19:13:54 +0200

    Added comments for bot and scale class

--- bot.py
+++ bot.py
@@ -5,10 +5,17 @@ import configparser
 
 class telegram_chatbot():
 
+    '''
+    Bot initialization. Pulls the bot API token from the config.ini file
+    '''
     def __init__(self, config):
         self.token = self.read_token_from_config_file(config)
         self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
+    '''
+    Bot updates. Timeout is set to 100 seconds, if updates are not given to the bot inside that time,
+    new timeout will be started
+    '''
     def get_updates(self, offset=None, r=None):
         url = self.base + "getUpdates?timeout=100"
         if offset:
@@ -26,11 +33,17 @@ class telegram_chatbot():
             r = requests.get(url)
         return json.loads(r.content)
 
+    '''
+    Sends a message back to the proper user / group
+    '''
     def send_message(self, msg, chat_id):
         url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
         if msg is not None:
             requests.get(url)
 
+    '''
+    Reads the bot API token from the config.ini file
+    '''
     def read_token_from_config_file(self, config):
         parser = configparser.SafeConfigParser()
         parser.read(config)
--- scale.py
+++ scale.py
@@ -1,9 +1,14 @@
 from hx711 import HX711
 import configparser
-import sys
 
 class Scale():
 
+    '''
+    Initialization for the scale class.
+    Params:
+            pin1, pin2 = GPIO pins the scale is hooked up to
+            reference_unit = the reference unit got from calibrating the scale according to the hx711 library
+    '''
     def __init__(self, pin1, pin2, reference_unit):
         self.hx = HX711(pin1, pin2)
         self.hx.set_reading_format("LSB", "MSB")
@@ -12,6 +17,9 @@ class Scale():
         self.hx.tare()
         self.parser = configparser.SafeConfigParser()
 
+    '''
+    Scale calibration. Saves the value measured to the config file
+    '''
     def calibrate(self, config, val):
         self.parser.read(config)
         val = str(val)
@@ -19,10 +27,16 @@ class Scale():
         with open('config.ini', 'w') as configfile:
             self.parser.write(configfile)
 
+    '''
+    Scale reset
+    '''
     def reset(self):
         self.hx.reset()
         self.hx.tare()
 
+    '''
+    Scale hard reset. Resets the scale and the configuration.
+    '''
     def hard_reset(self, config):
         self.parser.read(config)
         self.parser.set('creds', 'weight', '0')
@@ -31,11 +45,22 @@ class Scale():
         self.hx.reset()
         self.hx.tare()
 
+    '''
+    Fetching the pot weight from the configfile.
+    '''
     def get_pot_weight(self, config):
         self.parser.read(config)
         val = self.parser.getint('creds', 'weight')
         return val
 
+    '''
+    Returns all the values needed for the reply message
+    Return:
+            pot_weight = weight of the pot
+            val = weight on the scale
+            dl = desiliters of liquid in the pot
+            cups = how many 2dl cups of liquid there is
+    '''
     def get_values(self, config):
         pot_weight = self.get_pot_weight(config)
         val = max(0, int(self.hx.get_weight(5))) - pot_weight
